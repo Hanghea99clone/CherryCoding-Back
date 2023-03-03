44863cb7643925dc1f95efe992aef259d5ef70ba
DOGWANNA <9401ndk@naver.com>
2023-03-03 17:09:02 +0900

    feat: user 컨트롤러 추가

--- src/main/java/clone/cherrycoding/controller/UserController.java
+++ src/main/java/clone/cherrycoding/controller/UserController.java
@@ -1,4 +1,17 @@
 package clone.cherrycoding.controller;
 
+import clone.cherrycoding.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
+    private UserService userService;
+
+//    @PostMapping("/signup")
+
 }
