278000d0d7495c1072143fda44c8c1a32d455a77
HEUKWU <yoo4068@gmail.com>
2023-03-03 17:24:53 +0900

    Feat: ResponseDto

--- src/main/java/clone/cherrycoding/dto/ResponseDto.java
+++ src/main/java/clone/cherrycoding/dto/ResponseDto.java
@@ -1,5 +1,34 @@
 package clone.cherrycoding.dto;
 
-public class ResponseDto {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
+@Getter
+@AllArgsConstructor
+public class ResponseDto<T> {
+    private boolean success;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Error error;
+
+    public static <T> ResponseDto<T> success(T data) {
+
+        return new ResponseDto<>(true, data, null);
+    }
+    public static <T> ResponseDto<T> fail(int status, HttpStatus httpStatus, String message){
+        return new ResponseDto<>(false, null, new Error(status, httpStatus, message));
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class Error {
+        private final int status;
+        private final HttpStatus httpStatus;
+        private final String message;
+    }
 }
