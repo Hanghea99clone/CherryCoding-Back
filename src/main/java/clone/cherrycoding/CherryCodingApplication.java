927e8526d8e336f54c7f8426f7f29e449fef1170
HEUKWU <yoo4068@gmail.com>
2023-03-03 16:15:53 +0900

    remove source files

---
+++ src/main/java/clone/cherrycoding/CherryCodingApplication.java
@@ -0,0 +1,13 @@
+package clone.cherrycoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CherryCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clone/cherrycoding/CherryCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package clone.cherrycoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherryCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
