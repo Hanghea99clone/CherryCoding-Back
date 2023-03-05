8694b5a1ccf3de234704305570990b5b556e38eb
HEUKWU <yoo4068@gmail.com>
2023-03-05 11:27:15 +0900

    Feat: swaggerFix

--- src/main/java/clone/cherrycoding/controller/LectureController.java
+++ src/main/java/clone/cherrycoding/controller/LectureController.java
@@ -1,12 +1,11 @@
 package clone.cherrycoding.controller;
 
 import clone.cherrycoding.dto.*;
-import clone.cherrycoding.entity.User;
-import clone.cherrycoding.security.UserDetailsImpl;
 import clone.cherrycoding.security.UserDetailsServiceImpl;
 import clone.cherrycoding.service.LectureService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,51 +21,53 @@ public class LectureController {
     private final UserDetailsServiceImpl userDetailsService;
 
     @GetMapping("/lecture")
+    @Operation(summary = "홈페이지")
     public ResponseDto<List<LectureResponseDto>> getLecture() {
         return lectureService.getLecture();
     }
 
     @GetMapping("/curriculum")
+    @Operation(summary = "교육과정 모든강좌")
     public ResponseDto<List<CurriculumResponseDto>> getCurriculum(@RequestParam int page,
                                                                   @RequestParam int size,
-                                                                  @RequestParam(required = false, defaultValue = "createdAt") String sortBy,
-                                                                  @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        final User user = userDetailsService.getUser(userDetails);
-
-        return lectureService.getCurriculum(page - 1, size, sortBy, user);
+                                                                  @RequestParam(required = false, defaultValue = "createdAt") String sortBy) {
+        return lectureService.getCurriculum(page - 1, size, sortBy, userDetailsService.getUser());
     }
 
     @GetMapping("/curriculum/{curriculumId}")
-    public ResponseDto<DetailResponseDto> getDetail(@PathVariable Long curriculumId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        final User user = userDetailsService.getUser(userDetails);
+    @Operation(summary = "강좌 상세페이지")
+    public ResponseDto<DetailResponseDto> getDetail(@PathVariable Long curriculumId) {
 
-        return lectureService.getDetail(curriculumId, user);
+        return lectureService.getDetail(curriculumId, userDetailsService.getUser());
     }
 
     @GetMapping("/user-curriculum")
-    public ResponseDto<List<CurriculumResponseDto>> getUserCurriculum(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return lectureService.getUserCurriculum(userDetails.getUser());
+    @Operation(summary = "교육과정 내 강좌")
+    public ResponseDto<List<CurriculumResponseDto>> getUserCurriculum() {
+
+        return lectureService.getUserCurriculum(userDetailsService.getUser());
     }
 
-    @PostMapping("/lecture")
+    @PostMapping(value = "/lecture", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "강좌 추가")
     public ResponseDto<String> addLecture(@RequestPart LectureRequestDto requestDto,
-                                          @RequestPart(value = "image") MultipartFile multipartFile,
-                                          @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
+                                          @RequestPart(value = "image") MultipartFile multipartFile) throws IOException {
 
-        return lectureService.add(requestDto, multipartFile, userDetails.getUser());
+        return lectureService.add(requestDto, multipartFile, userDetailsService.getUser());
     }
 
-    @PutMapping("/curriculum/{curriculumId}")
+    @PutMapping(value = "/curriculum/{curriculumId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "강좌 수정")
     public ResponseDto<String> updateLecture(@PathVariable Long curriculumId,
                                              @RequestPart LectureRequestDto requestDto,
-                                             @RequestPart(value = "image") MultipartFile multipartFile,
-                                             @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
+                                             @RequestPart(value = "image") MultipartFile multipartFile) throws IOException {
 
-        return lectureService.update(curriculumId, requestDto, multipartFile, userDetails.getUser());
+        return lectureService.update(curriculumId, requestDto, multipartFile, userDetailsService.getUser());
     }
 
     @DeleteMapping("/curriculum/{curriculumId}")
-    public ResponseDto<String> deleteLecture(@PathVariable Long curriculumId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return lectureService.delete(curriculumId, userDetails.getUser());
+    @Operation(summary = "강좌 삭제")
+    public ResponseDto<String> deleteLecture(@PathVariable Long curriculumId) {
+        return lectureService.delete(curriculumId, userDetailsService.getUser());
     }
 }
--- src/main/java/clone/cherrycoding/controller/ReviewController.java
+++ src/main/java/clone/cherrycoding/controller/ReviewController.java
@@ -7,6 +7,7 @@ import clone.cherrycoding.entity.User;
 import clone.cherrycoding.repository.LectureRepository;
 import clone.cherrycoding.security.UserDetailsImpl;
 import clone.cherrycoding.service.ReviewService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -18,16 +19,19 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping("/{curriculumId}")
+    @Operation(summary = "댓글 작성")
     public ResponseDto<String> createReview(@PathVariable Long curriculumId, @RequestBody ReviewRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return reviewService.createReview(curriculumId, requestDto, userDetails);
     }
 
     @PutMapping("/{reviewId}")
+    @Operation(summary = "댓글 수정")
     public ResponseDto<String> updateReview(@PathVariable Long reviewId, @RequestBody ReviewRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return reviewService.updateReview(reviewId, requestDto, userDetails);
     }
 
     @DeleteMapping("/{reviewId}")
+    @Operation(summary = "댓글 삭제")
     public ResponseDto<String> deleteReview(@PathVariable Long reviewId, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return reviewService.deleteReview(reviewId, userDetails);
     }
